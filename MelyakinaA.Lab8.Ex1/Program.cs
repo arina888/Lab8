3375351b176513dfe94100d5dd10f876c5327ec7
arinameliakina <melyakina2003@mail.ru>
2022-02-24 12:37:51 +0300

    Добавьте файлы проекта.

---
+++ MelyakinaA.Lab8.Ex1/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Linq;
+
+string path = @"Text.txt";
+string pathDir = @"C:\Users\melya\source\repos\Lab8\Melyakina.Lab8.Ex1\obj\Debug\net6.0";
+DirectoryInfo dirInfo = new DirectoryInfo(pathDir);
+FileInfo info = new FileInfo(path);
+Console.WriteLine(info.FullName); //абсолютный путь файла
+Console.WriteLine(info.Name); // относительный путь файла
+Console.WriteLine(info.CreationTime); // дата создания файла
+Console.WriteLine($"{info.Length} байт"); // размер файла в байтах
+                                          //Console.WriteLine(info.);//есть ли кириллица,латиница,цифры,кол-во строк и слов
+Console.WriteLine();
+//Console.WriteLine(info.);
+
+
+Console.WriteLine(Path.GetFileNameWithoutExtension(path));
+Console.WriteLine(Environment.CurrentDirectory);
+
+Console.WriteLine();
+
+if (File.Exists(path))
+{
+    string text = File.ReadAllText(path);
+
+
+    int i = 0;
+    var words = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    for (i = 0; i < words.Length; i++)
+    {
+        i++;//количество слов
+
+
+
+    }
+
+    Console.WriteLine($"Всего слов:{i.ToString()}");
+
+    bool Flag1 = false;
+    bool Flag2 = false;
+    bool Flag3 = false;
+
+    for (int k = 0; k < text.Length; k++)
+    {
+
+        if (text[k] >= 'a' && text[k] <= 'z' || text[k] >= 'A' && text[k] <= 'Z')
+        {
+            Flag1 = true;
+
+
+        }
+        if (text[k] >= 'а' && text[k] <= 'я' || text[k] >= 'А' && text[k] <= 'Я')
+        {
+            Flag2 = true;
+
+
+        }
+        if (Char.IsDigit(text[k]))
+        {
+            Flag3 = true;
+
+        }
+
+
+    }
+    if (Flag3)
+    {
+        Console.WriteLine("В тексте есть цифры");
+    }
+    if (Flag2)
+    {
+        Console.WriteLine("В тексте есть кириллица");
+    }
+    if (Flag1)
+    {
+        Console.WriteLine("В тексте есть латиница");
+    }
+
+    var stat = words.Distinct()
+            .ToDictionary(word => word, word => words.Count(x => x == word))
+            .OrderByDescending(x => x.Value);
+    Console.WriteLine();
+
+    StringBuilder sb = new StringBuilder();
+    foreach (var item in stat)
+    {
+        Console.WriteLine(item);
+        sb.AppendLine($"{item.Key} {item.Value}");
+
+    }
+
+    StreamReader sr = new StreamReader("Text.txt");
+
+    int j = 0;
+    while ((text = sr.ReadLine()) != null) //читаем по одной линии(строке) пока не вычитаем все из потока (пока не достигнем конца файла)
+    {
+        j++;
+    }
+    Console.WriteLine("Всего строк:" + j.ToString());//количество строк
+
+    //СОХРАНЕНИЕ РЕЗУЛЬТАТОВ
+    sb.AppendLine($"{info.FullName}\n{info.Name}\n{info.CreationTime}\n{info.Length} байт\nВсего строк: {j.ToString()}\nВсего слов: {i.ToString()}");
+    //string result = $"{info.FullName}\n{info.Name}\n{info.CreationTime}\n{info.Length} байт\nВсего строк: {j.ToString()}\nВсего слов: {i.ToString()}";
+    //File.WriteAllText("result1.txt", result);
+    //Console.WriteLine(result);
+    File.WriteAllText("result1.txt", sb.ToString());
+
+}
+else
+{
+    Console.WriteLine("Такого файла не существует!");
+}
+//       Абсолютный путь.
+//    // string path = @"D:\Repo\Education\Files\Files\text1.txt";
+//    // Относительный путь.
+//    // string path = @"..\..\..\text1.txt";
---
+++ MelyakinaA.Lab8.Ex2/Program.cs
@@ -0,0 +1,85 @@
+﻿using System.Text;
+using System.Text.Encodings;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+
+var path = "Table.csv";
+//Регистрация провайдера кодировок.
+//  Делается один раз в приложении.
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+// Регистрация кодировки WINDOWS - 1251 для поддержки кирилицы.
+Encoding encoding = Encoding.GetEncoding(1251);
+
+
+var lines = File.ReadAllLines(path);
+Console.WriteLine(lines);
+var workers = new Worker[lines.Length - 1];
+
+for (int i = 1; i < lines.Length; i++)
+{
+    var splits = lines[i].Split(';');
+    var worker = new Worker();
+    worker.Фамилия = splits[0];
+    worker.Производительность_в_день = Convert.ToDouble(splits[1]);
+    worker.Время_работы_в_месяц = Convert.ToDouble(splits[2]);
+    worker.Стоимость_вазы = Convert.ToDouble(splits[3]);
+    workers[i - 1] = worker;
+    //Console.WriteLine(worker);
+}
+
+var result = "result.csv";
+using (StreamWriter streamWriter = new StreamWriter(result, false, encoding))
+{
+    streamWriter.WriteLine($"Name;Height;Weight;Coef");
+    for (int i = 0; i < workers.Length; i++)
+    {
+        streamWriter.WriteLine(workers[i].ToExcel());
+    }
+}
+
+var jsonOptions = new JsonSerializerOptions()
+{
+    Encoder = JavaScriptEncoder.Default
+};
+
+var json = JsonSerializer.Serialize(workers, jsonOptions);
+File.WriteAllText("result.json", json);
+
+var stringJson = File.ReadAllText("result.json");
+var array = JsonSerializer.Deserialize<Worker[]>(stringJson);
+foreach (var item in array)
+{
+    Console.WriteLine(item.ToString());
+}
+
+string jsonNewtonsoft = Newtonsoft.Json.JsonConvert.SerializeObject(workers, Newtonsoft.Json.Formatting.Indented);
+
+//Console.WriteLine(jsonNewtonsoft);
+File.WriteAllText("NewtonsoftResult.json", jsonNewtonsoft);
+
+//var readFile = File.ReadAllText("NewtonsoftResult.json");
+
+
+
+public class Worker
+{
+    public string Фамилия { get; set; }
+    public double Производительность_в_день { get; set; }
+    public double Время_работы_в_месяц { get; set; }
+
+    public double Стоимость_вазы { get; set; }
+
+    public double Количество_производимых_ваз { get => Время_работы_в_месяц * Производительность_в_день; }
+    public double Месячная_зарплата_работника { get => Количество_производимых_ваз * Стоимость_вазы; }
+    public override string ToString()
+    {
+        return $" {Фамилия}\n Производительность : {Производительность_в_день} ваз/в день\n Время работы: {Время_работы_в_месяц} дней в месяц\n Стоимость вазы: {Стоимость_вазы} рублей\n Количество производимых ваз:{Количество_производимых_ваз} в месяц\n Месячная зарплата работника:{Месячная_зарплата_работника} рублей\n";
+    }
+
+    public string ToExcel()
+    {
+        return $" {Фамилия};{Производительность_в_день};{Время_работы_в_месяц};{Стоимость_вазы};{Количество_производимых_ваз};{Месячная_зарплата_работника}";
+
+    }
+}
